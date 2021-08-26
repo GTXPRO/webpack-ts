6511779d0ff277e58d4d226a1843b93dc58830e4
Nguyễn Đình Đạt <nguyendinhdat@getflycrm.com>
2021-08-26 18:53:12 +0700

    add test component

---
+++ __test__/Hello.test.tsx
@@ -0,0 +1,35 @@
+/**
+ * @description   ¯\(ツ)/¯
+ * @since         Wednesday, 8 25th 2021, 8:36:45 am
+ * @author        Nguyễn Đạt <nguyendinhdat@getflycrm.com>
+ * @copyright     Copyright (c) 2021, GETFLY VN TECH.,JSC
+ * -----
+ * Change Log: <press Ctrl + alt + c write changelog>
+ */
+
+
+import React from "react";
+import { render, screen, cleanup } from "@testing-library/react";
+import '@testing-library/jest-dom';
+
+import Hello from './../src/Components/Hello';
+// import TestRenderer from 'react-test-renderer';
+
+describe("Test Component Hello By Text", () => {
+  it("String: \"Trần Văn Tú\" contains correct text", () => {
+    render(<Hello age={11} name="Trần Văn Tú" />);
+    expect(screen.getByText(/Trần Văn Tú/i)).toBeInTheDocument();
+  });
+
+  it("String: \"TranChemStore.com\" contains correct text", () => {
+    render(<Hello age={11} name="TranChemStore.com" />);
+    expect(screen.getByText(/TranChemStore\.com/i)).toBeInTheDocument();
+  });
+
+  it("Snapshot: \"Hello\" contains correct text", () => {
+    const { asFragment } = render(<Hello age={11} name="Hello" />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  afterEach(cleanup)
+});
---
+++ jest.config.ts
@@ -0,0 +1,33 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  moduleNameMapper: {
+    '\\.(css|less)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif)$': '<rootDir>/src/__mocks__/fileMock.ts'
+  },
+  verbose: true,
+  transform: {
+    '\\.[jt]sx?$': 'ts-jest',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/assetsTransformer.ts'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  testEnvironment: 'jsdom',
+  transformIgnorePatterns: [
+    'node_modules/(?!aggregate-error|clean-stack|escape-string-regexp|indent-string|p-map)'
+  ],
+  preset: 'ts-jest',
+  globals: {
+    window: {},
+    document: {},
+    'ts-jest': {
+      isolatedModules: true
+    }
+  },
+};
+
+export default config;
+
+// Or async function
+// export default async (): Promise<Config.InitialOptions> => config;
